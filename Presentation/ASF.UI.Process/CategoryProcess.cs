1afba863e266aad952f22a6c8b9a91378ace5897
AlejandroGomezUAI <30306375+AlejandroGomezUAI@users.noreply.github.com>
2017-09-01 14:54:59 -0300

    Ultima modificacion

--- Presentation/ASF.UI.Process/CategoryProcess.cs
+++ Presentation/ASF.UI.Process/CategoryProcess.cs
@@ -24,5 +24,28 @@ namespace ASF.UI.Process
             var response = HttpGet<AllResponse>("rest/Category/All", new Dictionary<string, object>(), MediaType.Json);
             return response.Result;
         }
+
+        public void insertCategory(Category category)
+        {
+            ProcessComponent.HttpPost<Category>("rest/Category/Add", category, MediaType.Json);
+        }
+
+        public Category findCategory(int id)
+        {
+
+            var response = HttpGet<FindResponse>("rest/Category/Find/{id}", MediaType.Json);
+            return response.Result;
+
+        }
+
+        public void editCategory(Category category)
+        {
+            ProcessComponent.HttpPost<Category>("rest/Category/Edit", category, MediaType.Json);
+        }
+
+        public void deleteCategory(Category category)
+        {
+            ProcessComponent.HttpPost<Category>("rest/Category/Remove", category, MediaType.Json);
+        }
     }
 }
